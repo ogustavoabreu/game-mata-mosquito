0deab42dd3649aa86c2004c7aa659d1aa38795ea
ogustavoabreu <66218221+ogustavoabreu@users.noreply.github.com>
2021-02-25 21:57:03 -0300

    Criação da lógica de regras do jogo

--- jogo.js
+++ jogo.js
@@ -1,6 +1,7 @@
 
 var altura = 0
 var largura = 0
+var vidas = 1
 
 function ajustaTamanhoPalcoJogo (){
     altura = window.innerHeight
@@ -16,7 +17,16 @@ function posicaoRandomica(){
     //remover o mosquito anterior (caso exista)
     if(document.getElementById('mosca')) {
         document.getElementById('mosca').remove()
-    }
+
+        if(vidas > 3){
+
+            alert('Interromper o jogo (game over)')
+        }else {
+            document.getElementById('v' + vidas).src="imagens/coracao_vazio.png"
+            vidas++
+            }
+        }
+
 
     var posicaoX = Math.floor(Math.random() * largura) - 90
     var posicaoY = Math.floor(Math.random() * altura) - 90
@@ -34,6 +44,9 @@ function posicaoRandomica(){
     mosca.style.top = posicaoY + 'px'
     mosca.style.position = 'absolute'
     mosca.id = 'mosca'
+    mosca.onclick = function (){
+        this.remove()
+    }
 
     document.body.appendChild(mosca)
 
